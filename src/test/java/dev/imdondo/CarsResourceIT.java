78dc94e8d16d4d81eec4fc4f9a804ce688c9aa51
Ignasio Dondo <ignasiodondo@standardbank.co.za>
2023-02-06 10:51:02 +0200

    removing unwanted files

--- src/test/java/dev/imdondo/CarsResourceIT.java
+++
@@ -1,8 +0,0 @@
-package dev.imdondo;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class CarsResourceIT extends CarsResourceTest {
-    // Execute the same tests but in packaged mode.
-}
